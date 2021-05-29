08afd24a782f4e06eb038cbdf373314ae075982f
Natalia <38763841+NataliaDPlesca@users.noreply.github.com>
2021-05-29 16:35:17 +0300

    Exemplele lectiei 23

---
+++ js/myScript_23.js
@@ -0,0 +1,7 @@
+//alert("JavaScript используется в веб-программировании".length);
+//alert(Math.abs(-77.88));
+//alert(Math.pow(9, 7));
+//alert(math.pow(9, 7));
+//alert(7*'a');
+console.log (5 + 7 * 77);
+alert(prompt("Сколько будет 2+2*2?")); 
