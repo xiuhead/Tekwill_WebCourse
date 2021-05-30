484e0a43889c9b61c571d7edaa2aa2dc2b02e0ed
Natalia <38763841+NataliaDPlesca@users.noreply.github.com>
2021-05-30 20:30:50 +0300

    Exemplele lectiei 24

---
+++ js/myScript_24.js
@@ -0,0 +1,44 @@
+/*
+var name = "Bob"; 
+var isMature = true; 
+var age = 35; 
+var result = '';
+var name = "Bob", isMature = true, age = 35, result = '';
+*/
+/*
+var myName = "Bob";
+var result = "";
+result = myName + ", is a programmer.";
+alert(typeof(67.9));
+alert(typeof(myName));
+alert(typeof(result));
+*/
+/*
+var myName = "Bob";
+var result = "";
+result = myName + ", is a programmer.";
+alert(result);
+*/
+/*
+var myName = "Bob";
+alert(myName + ", is a programmer.");
+*/
+/*
+var age = 35; 
+alert(Boolean(age>18));
+*/
+/*
+var age = 35; 
+alert(Boolean((age>18)&&(7<5)));
+*/
+/*
+var age = 35; 
+alert(Boolean(age==18));
+*/
+/*
+var age = 35; 
+alert(Boolean(age=='35'));
+*/
+var age = 35; 
+alert(Boolean(age==='35'));
+
